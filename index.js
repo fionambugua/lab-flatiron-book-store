1017a56e087ad2d5615f2d05ae7b836a270e6aaa
Fiona <fionambuguanjeri@gmail.com>
2026-07-07 15:55:33 +0300

    DOM manipulation code

--- index.js
+++ index.js
@@ -44,4 +44,26 @@ const bookStore = {
 }
 
 // Write your code here!
+const bookStoreTitle = document.getElementById("header");
+bookStoreTitle.textContent = bookStore.name;
 
+const bookList = document.getElementById("book-list");
+
+bookStore.books.forEach((book) => {
+  const bookContainer = document.createElement("li");
+
+  const bookTitle = document.createElement("h3");
+  bookTitle.textContent = book.title;
+
+  const bookAuthor = document.createElement("p");
+  bookAuthor.textContent = book.author;
+
+  const bookImage = document.createElement("img");
+  bookImage.src = book.imageUrl;
+
+  bookContainer.append(bookTitle, bookAuthor, bookImage);
+  bookList.append(bookContainer);
+});
+
+const deleteThis = document.getElementById("delete-this");
+deleteThis.remove();
